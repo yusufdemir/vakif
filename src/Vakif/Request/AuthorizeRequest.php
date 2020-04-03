f1f837a298c48e250194a559e52ef5515c41fc54
Yusuf Demir <yusufdemir@Yusufs-MacBook-Pro-2.local>
2020-04-03 15:34:56 +0300

    -dev: change authorize request params

--- src/Vakif/Request/AuthorizeRequest.php
+++ src/Vakif/Request/AuthorizeRequest.php
@@ -20,21 +20,24 @@ class AuthorizeRequest extends AbstractRequest
 
         /** @var HttpClient $httpClient */
         $httpClient = $this->getHttpClient();
+        $params = [
+          'MerchantId' => $token->getMerchantId(),
+          'MerchantPassword' => $token->getPassword(),
+          'VerifyEnrollmentRequestId' => $model->getOrderId(),
+          'Pan' => $model->getCreditCard()->getNumber(),
+          'ExpiryDate' => $model->getCreditCard()->getExpireYear().$model->getCreditCard()->getExpireMonth(),
+          'PurchaseAmount' => $model->getAmount(),
+          'Currency' => $model->getCurrency(),
+          'BrandName' => $model->getCardBrand(),
+          'SuccessUrl' => $model->getSuccessfulUrl(),
+          'FailureUrl' => $model->getFailureUrl(),
+          'SessionInfo' => $model->getAmount()
+        ];
+        if($model->getInstallment()>1){
+          $params['InstallmentCount'] = $model->getInstallment();
+        }
         $response = $httpClient->request('POST', $model->getBaseUrl(), [
-            'form_params' => [
-                'MerchantId' => $token->getMerchantId(),
-                'MerchantPassword' => $token->getPassword(),
-                'VerifyEnrollmentRequestId' => $model->getOrderId(),
-                'Pan' => $model->getCreditCard()->getNumber(),
-                'ExpiryDate' => $model->getCreditCard()->getExpireYear().$model->getCreditCard()->getExpireMonth(),
-                'PurchaseAmount' => $model->getAmount(),
-                'Currency' => $model->getCurrency(),
-                'BrandName' => $model->getCardBrand(),
-                'SuccessUrl' => $model->getSuccessfulUrl(),
-                'FailureUrl' => $model->getFailureUrl(),
-                'InstallmentCount' => $model->getInstallment(),
-                'SessionInfo' => $model->getAmount(),
-            ],
+            'form_params' => $params,
         ]);
 
         return new AuthorizeResponse($model, (array) @simplexml_load_string($response->getBody()->getContents()));
